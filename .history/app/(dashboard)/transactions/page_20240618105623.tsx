dd6fd9e9991eb1e56574dd7ccbf8b82b33a2fb19
= <=>
2024-06-18 11:07:41 -0500

    transaction account select

--- .history/app/(dashboard)/transactions/page_20240618105623.tsx
+++ .history/app/(dashboard)/transactions/page_20240618105623.tsx
@@ -1,23 +1,19 @@
 "use client";
 
-import { Loader2, Plus } from "lucide-react";
-import { useState } from "react";
-import { toast } from "sonner";
-
-import { DataTable } from "@/components/data-table";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Skeleton } from "@/components/ui/skeleton";
-import { transactions as transactionSchema } from "@/db/schema";
-import { useSelectAccount } from "@/features/accounts/hooks/use-select-account";
-import { useBulkCreateTransactions } from "@/features/transactions/api/use-bulk-create-transactions";
-import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
-import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
 import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
-
+import { Loader2, Plus } from "lucide-react";
 import { columns } from "./columns";
-import { ImportCard } from "./import-card";
+import { DataTable } from "@/components/data-table";
+import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+import { useState } from "react";
 import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
+import { transactions as transactionSchema } from "@/db/schema";
+import { toast } from "sonner";
 
 enum VARIANTS {
   LIST = "LIST",
@@ -27,19 +23,12 @@ enum VARIANTS {
 const INITIAL_IMPORT_RESULTS = {
   data: [],
   errors: [],
-  meta: [],
+  meta: {},
 };
 
 const TransactionsPage = () => {
   const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
   const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
-
-  const [AccountDialog, confirm] = useSelectAccount();
-  const newTransaction = useNewTransaction();
-  const createTransactions = useBulkCreateTransactions();
-  const deleteTransactions = useBulkDeleteTransactions();
-  const transactionsQuery = useGetTransactions();
-  const transactions = transactionsQuery.data || [];
 
   const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
     setImportResults(results);
@@ -50,6 +39,15 @@ const TransactionsPage = () => {
     setImportResults(INITIAL_IMPORT_RESULTS);
     setVariant(VARIANTS.LIST);
   };
+
+  const newTransaction = useNewTransaction();
+  const createTransactions = useBulkCreateTransactions();
+  const deleteTransactions = useBulkDeleteTransactions();
+  const transactionsQuery = useGetTransactions();
+  const transactions = transactionsQuery.data || [];
+
+  const isDisabled =
+    transactionsQuery.isLoading || deleteTransactions.isPending;
 
   const onSubmitImport = async (
     values: (typeof transactionSchema.$inferInsert)[]
@@ -72,20 +70,16 @@ const TransactionsPage = () => {
     });
   };
 
-  const isDisabled =
-    transactionsQuery.isLoading || deleteTransactions.isPending;
-
   if (transactionsQuery.isLoading) {
     return (
-      <div className="mx-auto -mt-6 w-full max-w-screen-2xl pb-10">
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
         <Card className="border-none drop-shadow-sm">
           <CardHeader>
             <Skeleton className="h-8 w-48" />
           </CardHeader>
-
           <CardContent>
-            <div className="flex h-[500px] w-full items-center justify-center">
-              <Loader2 className="size-6 animate-spin text-slate-300" />
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
             </div>
           </CardContent>
         </Card>
@@ -96,8 +90,6 @@ const TransactionsPage = () => {
   if (variant === VARIANTS.IMPORT) {
     return (
       <>
-        <AccountDialog />
-
         <ImportCard
           data={importResults.data}
           onCancel={onCancelImport}
@@ -108,13 +100,12 @@ const TransactionsPage = () => {
   }
 
   return (
-    <div className="mx-auto -mt-6 w-full max-w-screen-2xl pb-10">
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
         <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
-          <CardTitle className="line-clamp-1 text-xl">
-            Transaction History
+          <CardTitle className="text-xl line-clamp-1">
+            Transaction history
           </CardTitle>
-
           <div className="flex flex-col items-center gap-x-2 gap-y-2 lg:flex-row">
             <Button
               size="sm"
@@ -127,7 +118,6 @@ const TransactionsPage = () => {
             <UploadButton onUpload={onUpload} />
           </div>
         </CardHeader>
-
         <CardContent>
           <DataTable
             filterKey="payee"
